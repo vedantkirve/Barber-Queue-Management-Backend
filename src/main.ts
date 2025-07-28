5a59fa456c564f17e189be24a7757ce9b1a0660a
vedantkirve <pride02vmk@gmail.com>
2025-07-29 01:31:04 +0530

    added cors support

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // ✅ Frontend origin
+    credentials: true, // ✅ Allow sending cookies
+  });
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
