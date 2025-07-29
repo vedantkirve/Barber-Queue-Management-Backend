5569403db1db8184adf4c89f37a0b0ed33a140c5
vedantkirve <pride02vmk@gmail.com>
2025-07-30 01:26:55 +0530

    correct the key in validate

--- src/auth/jwt.stratergy.ts
+++ src/auth/jwt.stratergy.ts
@@ -16,6 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub };
+    console.log('payload-->>', payload);
+    return { userId: payload.userId };
   }
 }
