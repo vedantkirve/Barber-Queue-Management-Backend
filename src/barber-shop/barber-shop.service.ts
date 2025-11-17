5a02c00c490bf6e2700879b0aab9d84c8a9c7ee9
vedantkirve <pride02vmk@gmail.com>
2025-11-17 23:05:29 +0530

    added null

--- src/barber-shop/barber-shop.service.ts
+++ src/barber-shop/barber-shop.service.ts
@@ -137,7 +137,7 @@ export class BarberShopService {
             estimatedWaitTime: queueForThisShop.estimatedWaitTime,
             joinedAt: queueForThisShop.joinedAt,
           }
-        : {},
+        : null,
     };
   }
 
